b7806fa557bcd22e2155991d1a307dfd34b3beaf
Aayush Mittal <mittalaayush570@gmail.com>
2026-06-14 22:51:05 +0530

    chat tag fix

--- components/chats/UnifiedChatLayout.tsx
+++ components/chats/UnifiedChatLayout.tsx
@@ -641,7 +641,7 @@ export default function UnifiedChatLayout({ activeChatId }: UnifiedChatLayoutPro
                     </div>
                   </Link>
                 )}
-              </header>         </header>
+              </header>
 
               {/* Messages Feed Viewport */}
               <div className="flex-1 overflow-y-auto px-6 py-6 scrollbar-hide">
